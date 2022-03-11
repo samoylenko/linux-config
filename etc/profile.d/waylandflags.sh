75e162dab5bf7ebc400faca85f81b550a0cf2286
Michael Samoylenko <samoylenko@gmail.com>
2022-03-11 03:24:00 -0500

    Update QT settings

--- etc/profile.d/waylandflags.sh
+++ etc/profile.d/waylandflags.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export MOZ_ENABLE_WAYLAND=1
-export QT_QPA_PLATFORM=wayland
+export QT_QPA_PLATFORM="wayland;xcb"
+export QT_QPA_PLATFORMTHEME=qt5ct
 export _JAVA_AWT_WM_NONREPARENTING=1
 export CLUTTER_BACKEND=wayland
-export QT_QPA_PLATFORM=wayland
 export SDL_VIDEODRIVER=wayland
